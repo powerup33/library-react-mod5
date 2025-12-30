136f923c52d379ede168a561627376a4300de328
powerup33 <powerupcapital33@gmail.com>
2025-12-30 14:34:33 -0600

    nav complete

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCartShopping, faTimes } from "@fortawesome/free-solid-svg-icons";
 import LibraryLogo from "../assets/Library.svg";
 
 export default function Nav() {
@@ -24,16 +24,39 @@ export default function Nav() {
             </a>
           </li>
 
-            <button className="btn__menu">
-              <FontAwesomeIcon icon={faBars} />
-            </button>
+          <button className="btn__menu">
+            <FontAwesomeIcon icon={faBars} />
+          </button>
 
           <li className="nav__list">
-            <a href="/cart" className="nav__link">
+            <a href="/cart" className="nav__link cart__container">
               <FontAwesomeIcon icon={faCartShopping} />
+              <span className="cart__length">2</span>
             </a>
           </li>
         </ul>
+        <div className="menu__backdrop">
+            <button className="btn__menu btn__menu--close">
+                <FontAwesomeIcon icon={faTimes} />
+            </button>
+            <ul className="menu__links">
+                <li className="menu__list">
+                    <a href="/" className="menu__link">
+                        Home
+                    </a>
+                </li>
+                <li className="menu__list">
+                    <a href="/books" className="menu__link">
+                        Books
+                    </a>
+                </li>
+                <li className="menu__list">
+                    <a href="/cart" className="menu__link">
+                        Cart
+                    </a>
+                </li>
+            </ul>
+        </div>
       </div>
     </nav>
   );
