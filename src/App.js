10fb1848b573d4464fbd5481b7bbce9a0052a683
powerup33 <powerupcapital33@gmail.com>
2026-01-20 09:24:33 -0600

    Add to Cart

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ import Books from "./Pages/Books";
 import { books } from "./data";
 import BookInfo from "./Pages/BookInfo";
 import Cart from "./Pages/Cart";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book]);
+  }
+
+  useEffect(() => {
+    console.log("Cart updated:", cart);
+  }, [cart]); // <-- runs only when `cart` changes
+
   return (
     <Router>
       <div className="App">
@@ -15,9 +26,12 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/books" element={<Books books={books}/>} />
-          <Route path="/books/:id" element={<BookInfo books={books} />} />
-          <Route path="/cart" element={<Cart books={books} />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={<BookInfo books={books} cart={cart} addToCart={addToCart} />}
+          />
+          <Route path="/cart" element={<Cart books={books} cart={cart} />} />
         </Routes>
 
         <Footer />
